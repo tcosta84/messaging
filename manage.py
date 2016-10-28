913bab39892a605ae1a46289cea2cb6e2f15671a
Thiago Costa <thiagodacosta@gmail.com>
2016-10-28 03:31:34 -0200

    Adiciona script

---
+++ manage.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from flask_script import Manager, Server
+from flask_migrate import MigrateCommand
+
+from messaging import create_app
+
+app = create_app()
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+manager.add_command('runserver', Server(threaded=True))
+
+if __name__ == '__main__':
+    manager.run()
